7db00fd05bc50b133aa5e03f62acdf71d2870916
SandeepShakya <sandeepshakya2015@gmail.com>
2025-03-12 16:39:38 +0530

    Adding Dog vs cat classfiers

--- 6_NewCourse/2_CIFAR10.py
+++ 6_NewCourse/2_CIFAR10.py
@@ -1,5 +1,1 @@
-import torch, torchvision
-from torchvision import datasets, transforms
-
-train_data = datasets.CIFAR10(root="data", train=True, download=True, transform=transforms.ToTensor())
-test_data = datasets.CIFAR10(root="data", train=False, download=True, transform=transforms.ToTensor())
+ 
