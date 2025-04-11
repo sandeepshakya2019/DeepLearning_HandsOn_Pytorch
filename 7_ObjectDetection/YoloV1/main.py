5af3d546b29e92159f8e25900481d72f36312050
SandeepShakya <sandeepshakya2015@gmail.com>
2025-04-11 12:09:09 +0530

    Changes

--- 7_ObjectDetection/YoloV1/main.py
+++
@@ -1,8 +0,0 @@
-import package
-from model import YOLOv1
-from torchinfo import summary
-
-model = YOLOv1(num_classes=20, num_anchors=2)
-summary(model, input_size=(1, 3, 448, 448))
-
-print("[+] Summary Genrated")
